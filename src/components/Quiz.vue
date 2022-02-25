bfe040ae69bfc1eec6cff5662641961a48be8970
jyliao369 <jyliao369@gmail.com>
2022-02-24 22:13:13 -0500

    New categories can be added. need to rework the add questions function

--- src/components/Quiz.vue
+++ src/components/Quiz.vue
@@ -7,7 +7,8 @@
   <div class="addQues">
     <!-- <button @click="addQuiz">Add New Quiz</button> -->
     <button @click="addQuestion">Add Question</button>
-    <button @click="testScore">test</button>
+    <button @click="addCategory">Add New Category</button>
+    <!-- <button @click="addQuiz">test</button> -->
   </div>
 
   <div class="overCont">
@@ -15,7 +16,12 @@
       <!-- THIS IS THE SCOPRE SECTION -->
       <div class="infoHead">
         <div>
-          <h2>Question {{ b }}/{{ this.questions.length }}</h2>
+          <h2>{{ currentCategory[0].name }}</h2>
+        </div>
+        <div>
+          <h2>
+            Question {{ b }}/{{ this.currentCategory[0].questions.length }}
+          </h2>
         </div>
         <div>
           <h2>Score: {{ score }}</h2>
@@ -25,28 +31,29 @@
       <div class="quesContainer">
         <div
           class="question"
-          v-for="(question, index) in questions.slice(a, b)"
+          v-for="(questions, index) in currentCategory[0].questions.slice(a, b)"
           :key="index"
         >
           <div class="quesTitle">
-            <p>{{ question.question }}</p>
+            <p>{{ questions.question }}</p>
           </div>
           <div class="choiceCont">
             <div class="choice">
-              <p @click="checkCorrect('A')">A: {{ question.ansA }}</p>
+              <p @click="checkCorrect('A')">A: {{ questions.ansA }}</p>
             </div>
             <div class="choice">
-              <p @click="checkCorrect('B')">B: {{ question.ansB }}</p>
+              <p @click="checkCorrect('B')">B: {{ questions.ansB }}</p>
             </div>
             <div class="choice">
-              <p @click="checkCorrect('C')">C: {{ question.ansC }}</p>
+              <p @click="checkCorrect('C')">C: {{ questions.ansC }}</p>
             </div>
             <div class="choice">
-              <p @click="checkCorrect('D')">D: {{ question.ansD }}</p>
+              <p @click="checkCorrect('D')">D: {{ questions.ansD }}</p>
             </div>
           </div>
         </div>
       </div>
+
       <!-- THIS IS THE BUTTON SECTION -->
       <div class="btnContainer">
         <div class="btnBackground">
@@ -59,7 +66,7 @@
         </div>
         <div class="btnBackground">
           <button
-            v-if="this.b <= this.questions.length - 1"
+            v-if="this.b <= this.currentCategory[0].questions.length - 1"
             :disabled="false"
             @click="nextQuestion"
           >
@@ -69,22 +76,19 @@
         </div>
       </div>
     </div>
-  </div>
 
-  <!-- <div class="outContainer">
-    <div class="container">
-      
+    <!-- THIS IS HOLDS ALL OF THE CATEGORIES  -->
+    <div class="catContainer">
+      <div v-for="(category, index) in categories" :key="index">
+        <button @click="changeCategory(index)">{{ category.name }}</button>
+      </div>
     </div>
-  </div> -->
-  <!-- <div
-    class="quizContainer"
-    v-for="(quiz, index) in testArray"
-    :key="index"
-    v-cloak
-  >
-    {{ testArray[index] }}
-    {{ testArray[index].length }}
-  </div> -->
+  </div>
+  <br />
+  <br />
+  <br />
+  <br />
+  <br />
 </template>
 
 <script>
@@ -96,13 +100,20 @@ export default {
   },
   data() {
     return {
-      showModal: false,
-      testArray: [
+      currentCategory: [
         {
-          Chemistry: [],
-        },
-        {
-          Biology: [],
+          name: "Biology",
+          questions: [
+            {
+              question:
+                "If the transformation depicted in organelle B requires oxygen, what form of energy is represented by E IV?",
+              ansA: "Radiant energy in the form of photons",
+              ansB: "Chemical energy being stored as glycogen",
+              ansC: "Chemical energy in the form of ATP",
+              ansD: "Chemical energy released by glycolysis",
+              answer: "A",
+            },
+          ],
         },
       ],
       questions: [
@@ -116,6 +127,95 @@ export default {
           answer: "A",
         },
       ],
+      categories: [
+        {
+          name: "Chemistry",
+          questions: [
+            {
+              question: "Which of the following has a positive charge?",
+              ansA: "proton",
+              ansB: "neutron",
+              ansC: "anion",
+              ansD: "electron",
+              answer: "A",
+            },
+            {
+              question:
+                "Rutherford carried out experiments in which a beam of alpha particles was directed at a thin piece of metal foil. From these experiments he concluded that:",
+              ansA: "electrons are massive particles.",
+              ansB: "the positively charged parts of atoms are moving about with a velocity approaching the speed of light.",
+              ansC: "the positively charged parts of atoms are extremely small and extremely heavy particles.",
+              ansD: "the diameter of an electron is approximately equal to that of the nucleus.",
+              answer: "C",
+            },
+          ],
+        },
+        {
+          name: "Math",
+          questions: [
+            {
+              question: "What is three fifth of 100?",
+              ansA: "3",
+              ansB: "5",
+              ansC: "20",
+              ansD: "60",
+              answer: "D",
+            },
+            {
+              question: "What is the remainder of 21 divided by 7?",
+              ansA: "0",
+              ansB: "7",
+              ansC: "1",
+              ansD: "21",
+              answer: "A",
+            },
+          ],
+        },
+        {
+          name: "Trivia",
+          questions: [
+            {
+              question: "What is the national language of Canada?",
+              ansA: "English",
+              ansB: "Dutch",
+              ansC: "Chinese",
+              ansD: "French",
+              answer: "D",
+            },
+            {
+              question: "Saudi Arabia is the biggest producer of?",
+              ansA: "Coffee",
+              ansB: "Oil",
+              ansC: "Coal",
+              ansD: "Bread",
+              answer: "B",
+            },
+          ],
+        },
+      ],
+      newCategory: {
+        name: "History",
+        questions: [
+          {
+            question:
+              "Which of these countries did the Soviet Union NEVER invade?",
+            ansA: "Afghanistan",
+            ansB: "Finland",
+            ansC: "Poland",
+            ansD: "Sweden",
+            answer: "D",
+          },
+          {
+            question: "Who was the first person to orbit the Earth?",
+            ansA: "Neil Armstrong",
+            ansB: "The Boss",
+            ansC: "Yuri Gagarin",
+            ansD: "John Glenn",
+            answer: "C",
+          },
+        ],
+      },
+      showModal: false,
       a: 0,
       b: 1,
       index: 0,
@@ -153,14 +253,11 @@ export default {
       console.log(this.questions[this.index].answer);
       if (answer === this.questions[0].answer) {
         console.log("Correct");
+        this.score = this.score + 100;
       } else {
         console.log("Incorrect");
+        this.score = this.score - 100;
       }
-    },
-    addQuiz() {
-      let Math = [];
-      this.testArray.push({ Math });
-      console.log(this.testArray);
     },
     testHandle() {
       this.testArray[0].test1.push({ question: "hello", reponse: "goodbye" });
@@ -168,8 +265,14 @@ export default {
       console.log(this.testArray[0].test1[0]);
       console.log(this.testArray[0].test2[0]);
     },
-    testScore() {
-      this.score = this.score + 100;
+    changeCategory(number) {
+      console.log("index value");
+      console.log(number);
+      this.currentCategory = [];
+      this.currentCategory.push(this.categories[number]);
+    },
+    addCategory() {
+      this.categories.push(this.newCategory);
     },
   },
 };
@@ -183,8 +286,10 @@ export default {
 }
 .overCont {
   display: flex;
+  flex-direction: column;
   justify-content: center;
   margin: 15px;
+  align-items: center;
 }
 .container {
   width: 450px;
@@ -192,6 +297,7 @@ export default {
   padding: 20px;
   background: #33628a;
   border-radius: 10px;
+  margin-bottom: 20px;
 }
 .btnContainer {
   display: flex;
@@ -253,8 +359,33 @@ export default {
   margin: 10px;
   background: #d9e2e9;
 }
+.choice:hover {
+  background: #706eeb;
+  color: white;
+}
 .choice p {
   margin: 7px;
   font-size: 18px;
+}
+.catContainer {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  width: 450px;
+  font-family: "Source Sans 3";
+  padding: 20px;
+  background: #33628a;
+  border-radius: 10px;
+}
+.catContainer button {
+  width: 125px;
+  font-size: 20px;
+  border-radius: 15px;
+  padding: 15px;
+  margin: 10px;
+  border: none;
+  background: #c2d0dc;
+  color: #3f3f3f;
 }
 </style>
